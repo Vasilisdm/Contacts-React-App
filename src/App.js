145733f51742ff06bad58a81fd6304d82cab2290
= <=>
2018-08-26 20:43:16 +0300

    added state to the App class

--- src/App.js
+++ src/App.js
@@ -2,33 +2,35 @@ import React, { Component } from 'react';
 // Importing ListContacts component
 import ListContacts from './ListContacts';
 
-const contacts = [
-  {
-    "id": "ryan",
-    "name": "Ryan Florence",
-    "email": "ryan@reacttraining.com",
-    "avatarURL": "http://localhost:5001/ryan.jpg"
-  },
-  {
-    "id": "michael",
-    "name": "Michael Jackson",
-    "email": "michael@reacttraining.com",
-    "avatarURL": "http://localhost:5001/michael.jpg"
-  },
-  {
-    "id": "tyler",
-    "name": "Tyler McGinnis",
-    "email": "tyler@reacttraining.com",
-    "avatarURL": "http://localhost:5001/tyler.jpg"
-  }
-]
 
 class App extends Component {
+  state = {
+     contacts: [
+      {
+        "id": "ryan",
+        "name": "Ryan Florence",
+        "email": "ryan@reacttraining.com",
+        "avatarURL": "http://localhost:5001/ryan.jpg"
+      },
+      {
+        "id": "michael",
+        "name": "Michael Jackson",
+        "email": "michael@reacttraining.com",
+        "avatarURL": "http://localhost:5001/michael.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "email": "tyler@reacttraining.com",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      }
+    ]
+  }
   render() {
     return (
       <div className="App">
       {/* rendering the ListContacts component */}
-        <ListContacts contacts={contacts}/>
+        <ListContacts contacts={this.state.contacts}/>
       </div>
     );
   }
