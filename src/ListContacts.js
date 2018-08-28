e42e128650a4c647deb303d29e5af0c70e752e60
= <=>
2018-08-28 08:13:44 +0300

    removed the commented class ContactList

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -40,39 +40,4 @@ ListContacts.propTypes = {
     onDeleteContact: PropTypes.func.isRequired
 }
 
-// class ListContacts extends Component {
-//     render(){
-//         return (
-//             <ol className='contact-list'>
-//             {/* mapping over the contacts attribute in order to display the names in the list */}
-//                 {this.props.contacts.map(contact => (
-
-//                     // Adding key attribute in order for react to know which li item has changed 
-//                     // rather than recreating the whole ol each time.
-//                     // This property makes the unique key prop warning in the console to go away.
-//                     <li key={contact.id} className='contact-list-item'>
-
-//                         <div className='contact-avatar' style={{
-//                             backgroundImage: `url(${contact.avatarURL})`
-//                             }}>
-//                         </div>
-
-//                         {/* showing each contact's name and email*/}
-//                         <div className='contact-details'>
-//                             <p>{contact.name}</p>
-//                             <p>{contact.email}</p>
-//                         </div>
-
-//                         <button className='contact-remove'>
-//                             Remove
-//                         </button>
-
-//                     </li>
-//                 ))}
-//             </ol>
-//         )
-//     }
-// }
-
-
 export default ListContacts
