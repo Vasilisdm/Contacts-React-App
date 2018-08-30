df8cfe8a2217cba0fcc90a32ff4c61d265c90f19
= <=>
2018-08-30 07:25:18 +0300

    newly created createContact component

---
+++ src/createContact.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class CreateContact extends Component {
+    render() {
+        <div>
+            <h1>Create a contact item</h1>
+        </div>
+    }
+}
+
+
+export default CreateContact
