4ec4060304fba5b0918a0734ca40b86bdb0244f7
= <=>
2018-08-30 07:27:26 +0300

    added return to createContact component

--- src/createContact.js
+++ src/createContact.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 
 class CreateContact extends Component {
     render() {
-        <div>
-            <h1>Create a contact item</h1>
-        </div>
+        return(
+            <div>
+                <h1>Create a contact item</h1>
+            </div>
+        )
     }
 }
 
